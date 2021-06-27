0279fbffa3326a66512968fdd80b4fc9598e0def
omrior <55718926+omrior@users.noreply.github.com>
2021-06-27 11:04:44 +0300

    Enf of chapter 49: added angular form with login.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'The Dating App';
+  title = 'Omri\'s App';
   users: any;
 
   constructor(private http: HttpClient) {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,20 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';  // Added
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';  // Added
+import {FormsModule} from '@angular/forms'
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,  // Added
-    BrowserAnimationsModule //Added 
+    BrowserAnimationsModule, //Added 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
