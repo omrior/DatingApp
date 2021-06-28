46ea3173b3a7f8d0c1a2c8599acc1aaab0408af0
omrior <55718926+omrior@users.noreply.github.com>
2021-06-28 11:14:43 +0300

    End of chapter 66: routing in angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,13 +11,21 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    ListsComponent,
+    MessagesComponent,
+    MemberListComponent,
+    MemberDetailComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,15 +13,14 @@ export class NavComponent implements OnInit {
   model: any = {}
   // currentUser$: Observable<User>;  // We can remove this property
 
-  constructor(public accountService: AccountService) { } // Constructor is changed from private to public
+  constructor(public accountService: AccountService, private router: Router) { } // Constructor is changed from private to public
 
   ngOnInit(): void {
-    // this.currentUser$ = this.accountService.currentUser$; //// We can remove this property
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members');
       //this.loggedIn = true;
     }, error => {
       console.log(error);
@@ -29,7 +29,7 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    //this.loggedIn = false;
+    this.router.navigateByUrl('/');
   }
 
   /* getCurrentUser() {
